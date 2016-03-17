2ccc752c6df6e19c77b8c869282ea7baa87eb37a
shirleylberry <shirleylberry@gmail.com>
2016-03-17 18:03:56 -0400

    added a random gif on the front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
+require 'net/http'
+require 'json'
+
 class PagesController < ApplicationController
   def home
+    #ruby
+    # http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho
+    url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=pizza"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    buffer = resp.body
+    result = JSON.parse(buffer) 
+    # byebug
+    @image = result["data"]["fixed_height_downsampled_url"]
   end
 end
