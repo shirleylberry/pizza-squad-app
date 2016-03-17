de4c0aa96e31c200d27a56067338e5303d2e5299
shirleylberry <shirleylberry@gmail.com>
2016-03-17 11:09:44 -0400

    validations for orders

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,35 +9,38 @@
 #  updated_at :datetime         not null
 #
 
-require "rspec"
-require_relative "../lib/pizzasquad"
+require_relative "../spec_helper.rb"
 
 RSpec.describe Order, type: :model do
-  order1 = Order.create(user_id: "1", event_id: "1")
+  let(:user) {User.create(name: "Lucille Ball", email: "lucille@ilovelucy.com", password: "riiiicky")}
+  let(:pres_user) {User.create(name: "Ricky Ricard", email: "ricky@ilovelucy.com", password: "LUUUCY")}
+  let(:president) {President.create(user: pres_user)}
+  let(:event) {Event.create(date: "2014-09-19 07:03:30 -0700", deadline: "2014-09-19 05:03:30 -0700", title: "PIZZA NOW", description: "YOU HEARD ME", president: president)}
+  
+  let!(:order) {Order.create(user: user, event: event)}
 
-describe 'belongs_to associations' do
-  let(:order) {Order.new}
-  let(:event) {Event.new}
-  let(:user) {User.new}
+  let(:no_user_attrs) {{event: event}}
+  let(:no_event_attrs) {{user: user}}
 
+  describe 'belongs_to associations' do
     it 'belongs to an event' do
-      order.event = event
       expect(order.event).to eq(event)
     end
 
     it 'belongs to a user' do
-      order.user = user
       expect(order.user).to eq(user)
     end
+  end
 
-  describe 'attributes' do
-
-    it 'has a user_id' do
-      expect(order1.user_id).to eq("1")
+  describe 'validations' do 
+    it 'is invalid without a user' do
+      new_order = Order.new(no_user_attrs)
+      expect(new_order.save).to eq(false)
     end
 
-    it 'has an event_id' do
-      expect(order1.event_id)to eq("1")
+    it 'is invalid without an event' do 
+      new_order = Order.new(no_event_attrs)
+      expect(new_order.save).to eq(false)
     end
   end
 end
