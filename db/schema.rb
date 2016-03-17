24187db3a6878d1c389f8bc7f076eb47f51d4400
shirleylberry <shirleylberry@gmail.com>
2016-03-17 14:29:18 -0400

    fixed migration files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317153517) do
+ActiveRecord::Schema.define(version: 20160317135715) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
