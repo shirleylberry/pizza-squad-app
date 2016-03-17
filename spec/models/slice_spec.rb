1643a0586b6412439cbc9a7fcdd0f8908e9d2be9
shirleylberry <shirleylberry@gmail.com>
2016-03-17 10:55:08 -0400

    added setup and validations to slice_spec

--- spec/models/slice_spec.rb
+++ spec/models/slice_spec.rb
@@ -9,37 +9,43 @@
 #  updated_at :datetime         not null
 #
 
-require "rspec"
-require_relative "../lib/pizzasquad"
+require_relative "../spec_helper.rb"
 
 RSpec.describe Slice, type: :model do
-  slice = Slice.create(pizza_id: "1", order_id: "1")
+  let(:user) {User.create(name: "Lucille Ball", email: "lucille@ilovelucy.com", password: "riiiicky")}
+  let(:pres_user) {User.create(name: "Ricky Ricard", email: "ricky@ilovelucy.com", password: "LUUUCY")}
+  let(:president) {President.create(user: pres_user)}
+  let(:event) {Event.create(date: "2014-09-19 07:03:30 -0700", deadline: "2014-09-19 05:03:30 -0700", title: "PIZZA NOW", description: "YOU HEARD ME", president: president)}
+  let(:order) {Order.create(user: user, event: event)}
+  let(:pizza) {Pizza.create(topping: "pepperoni", price: 15.0)}
+
+  let!(:slice) {Slice.create(order: order, pizza: pizza)}
+
+  let(:no_pizza_attributes) {{order: order}}
+  let(:no_order_attributes) {{pizza: pizza}}
 
   describe 'belongs_to associations' do
-    let(:slice) {Slice.new}
-    let(:pizza) {Pizza.new}
-    let(:order) {Order.new}
-
-      it 'belongs to a pizza' do
-        slice.pizza = pizza
-        expect(slice.pizza).to eq(pizza)
-      end
-
-      it 'belongs to an order' do
-        slice.order = order
-        expect(slice.order).to eq(order)
-      end
-
-  describe 'attributes' do
-
-    it 'has a pizza_id' do
-      expect(slice.pizza_id).to eq("1")
+    it 'belongs to a pizza' do
+      slice.pizza = pizza
+      expect(slice.pizza).to eq(pizza)
     end
 
-    it 'has an order_id' do
-      expect(slice.pizza_id)to eq("1")
+    it 'belongs to an order' do
+      slice.order = order
+      expect(slice.order).to eq(order)
+    end
+  end
+
+  describe 'validations' do
+    it 'is invalid without a pizza' do
+      new_slice = Slice.new(no_pizza_attributes)
+      expect(new_slice.save).to eq(false)
     end
 
-
+    it 'is invalid without an order' do
+      new_slice = Slice.new(no_order_attributes)
+      expect(new_slice.save).to eq(false)
+    end
+  end
 end
 
