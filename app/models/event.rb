4fbad42fe3016209ef289c37d13e3d13a0f96f86
shirleylberry <shirleylberry@gmail.com>
2016-03-18 17:54:31 -0400

    event can calculate its total price

--- app/models/event.rb
+++ app/models/event.rb
@@ -71,7 +71,6 @@ class Event < ActiveRecord::Base
     end
   end
 
-<<<<<<< HEAD
   # pizza logic
   def determine_remainder(number)
     if number%8 != 0 && number%8 < 4
@@ -79,6 +78,8 @@ class Event < ActiveRecord::Base
     elsif number%8 != 0 && number%8 >= 4
       remainder = 0
       number += number % 8
+    else
+      remainder = 0
     end
     num_slices = remainder
     num_pies = number / 8
@@ -91,9 +92,11 @@ class Event < ActiveRecord::Base
     pies_slices_hash = {pies: {}, slices: {}}
     order_hash = pie_types_hash.each_with_object(pies_slices_hash) do |(topping, number), pies_slices_hash|
       num_slices, num_pies = determine_remainder(number)
+      # byebug
       pies_slices_hash[:slices][topping] = num_slices unless num_slices == 0
       pies_slices_hash[:pies][topping] = num_pies
     end
+    order_hash
   end
 
   def get_pie_types_counts_hash
@@ -107,6 +110,42 @@ class Event < ActiveRecord::Base
         old_val + new_val
       end
     end
+  end
+
+  def total_slices_price(slices_hash)
+    slices_total = 0
+    slices_hash.each do |topping, number|
+      pizza = Pizza.find_by_topping(topping)
+      slices_total += pizza.indiv_piece_price * number
+    end
+    slices_total
     # byebug
+  end
+
+  def total_pies_price(pies_hash)
+    pies_total = 0
+    pies_hash.each do |topping, number|
+      pizza = Pizza.find_by_topping(topping)
+      pies_total += pizza.price * number
+    end
+    pies_total
+  end
+
+  def total_price
+    slices_pies = self.determine_slices_pies
+    total = 0
+    slices_pies.each do |item_type, item_hash|
+      # iterate over all of them, find the pizza with that topping
+      # add either the pie price or the indiv_piece_price to the total
+      # byebug
+      if item_type == :pies
+        total += total_pies_price(item_hash)
+      elsif item_type == :slices
+        total += total_slices_price(item_hash)
+      end
+    end
+    # byebug
+    total
+  end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -15,11 +15,13 @@
 require_relative "../spec_helper.rb"
 
 RSpec.describe Event, type: :model do
+  DatabaseCleaner.clean
   let!(:user) {User.create(name: "Jeff Winger", email: "jeff@coollawyer.com", password: "pierceisweird")}
   let!(:pres_user) {User.create(name: "Britta Perry", email: "britta@activism.com", password: "savethewhales")}
   let!(:president) {President.create(user: pres_user)}
 
   let!(:event) {Event.create(date: "2016-09-19 07:03:30 -0700", deadline: "2016-09-19 05:03:30 -0700", title: "FSP Pizza Party", description: "yum pizza yum", president: president)}
+  let!(:event2) {Event.create(date: "2016-11-22 07:03:30 -0700", deadline: "2016-11-22 03:03:30 -0700", title: "Awesome Party", description: "fun times", president: President.last)}
 
   let(:invalid_date_params) {{date: "2016-09-19 07:03:30 -0700", deadline: "2016-09-10 05:03:30 -0700", title: "FSP Pizza Party", description: "yum pizza yum", president: president}}
   let(:no_title_params) {{date: "2014-09-19 07:03:30 -0700", deadline: "2014-09-19 05:03:30 -0700", description: "yum pizza yum", president: president}}
@@ -29,7 +31,7 @@ RSpec.describe Event, type: :model do
 
   describe 'attributes' do
     it 'has a president' do
-      expect(event.president).to eq(president) # unsure of date format
+      expect(event.president).to eq(president) 
     end
 
     it 'has a title' do
@@ -37,7 +39,7 @@ RSpec.describe Event, type: :model do
     end
 
     it 'has a date' do
-      expect(event.date).to eq("2016-09-19 14:03:30.000000000 +0000") # unsure of date format
+      expect(event.date).to eq("2016-09-19 14:03:30.000000000 +0000") 
     end
 
     it 'has a description' do
@@ -45,7 +47,7 @@ RSpec.describe Event, type: :model do
     end
 
     it 'has a deadline' do
-      expect(event.deadline).to eq("2016-09-19 05:03:30 -0700") # same as above - unsure of date format
+      expect(event.deadline).to eq("2016-09-19 05:03:30 -0700") 
     end
   end
 
@@ -92,6 +94,24 @@ RSpec.describe Event, type: :model do
       # {"cheese": 18, mushroom: "17", "pepperoni": 13}
       slices_pies = event.determine_slices_pies
       expect(slices_pies).to eq({:pies=>{"cheese"=>2, "Mushroom"=>2, "pepperoni"=>2}, :slices=>{"cheese"=>2, "Mushroom"=>1}})
+    end
+  end
+
+  describe '#total_price' do 
+    it 'returns the total price for the event' do
+      6.times{User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)}
+      # byebug
+      30.times{Order.create(user: User.all.sample, event: event2)}
+
+      @cheese_pizza = Pizza.create(topping: "nachos", price: 5.0, indiv_piece_price: 1.0)
+      @shroom_pizza = Pizza.create(topping: "ground beef", price: 10.0, indiv_piece_price: 2.0)
+      @pepp_pizza = Pizza.create(topping: "tomatoes", price: 15.0, indiv_piece_price: 2.5)
+
+      15.times{Slice.create(order: Order.all.sample, pizza: @cheese_pizza)}
+      8.times{Slice.create(order: Order.all.sample, pizza: @shroom_pizza)}
+      10.times{Slice.create(order: Order.all.sample, pizza: @pepp_pizza)}
+      # byebug
+      expect(event2.total_price).to eq(40.0)
     end
   end
 
